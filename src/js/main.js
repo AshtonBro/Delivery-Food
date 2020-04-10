32f2c262f9e25b84193eed01be5b173185ff3161
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-04-10 23:08:26 +0500

    fech

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@
 const cardBtnBasket = document.getElementById('card-button-basket');
 const modal = document.querySelector('.modal');
 const closeBtn = document.querySelector('.modal-close');
+// const buttonActive = document.getElementsByClassName('button-active');
 
 const toggleModal = () => {
     modal.classList.toggle("modal-active");
@@ -12,3 +13,6 @@ new WOW().init();
 
 cardBtnBasket.addEventListener('click', toggleModal);
 closeBtn.addEventListener('click', toggleModal);
+
+// buttonActive.forEach(elem => elem.addEventListener('click', toggleModal));
+
